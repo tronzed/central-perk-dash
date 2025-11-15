f18be94ac2796a4df8a9add1d13f7b6fa7a39fb1
tanuj <your_email@example.com>
2025-11-15 18:10:28 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 import './App.css'
 import Home from './pages/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -8,17 +11,32 @@ function App() {
 
   return (
     <>
-     
 
-    <BrowserRouter>
-    
-      <Routes>
 
-        <Route path="/" element={<Home/>} />
 
-      </Routes>
-    
-    </BrowserRouter>
+
+
+
+      <BrowserRouter>
+
+        <div id="app">
+          <div className="main-wrapper main-wrapper-1">
+
+            <Header />
+            <Sidebar />
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+
+            <Footer />
+
+          </div>
+        </div>
+      </BrowserRouter>
+
+
+
 
 
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+export default function footer() {
+
+    return (
+
+        <>
+
+            <footer className="main-footer">
+                <div className="footer-left">
+                    Copyright © 2018 <div className="bullet" /> Design By{" "}
+                    <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+                </div>
+                <div className="footer-right"></div>
+            </footer>
+
+        </>
+
+    );
+
+}
